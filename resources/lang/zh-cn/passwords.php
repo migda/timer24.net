dea6356bf63c73621cadbbad96ceebbbc8fb8f9e
singhigh <singhigh@users.noreply.github.com>
2016-10-24 13:51:07 +0800

    Create passwords.php

---
+++ resources/lang/zh-cn/passwords.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => '密码至少要六位，且两次输入必须一样！',
+    'reset' => '您的密码已重置。',
+    'sent' => '系统已将密码重置链接发送到您的邮箱',
+    'token' => '您的密码重置链接无效，请重新获取。',
+    'user' => "无此用户，请确认后重试。",
+];
